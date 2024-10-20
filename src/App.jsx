2ad614529646e468388b1fd80118036285532019
Kel Kel <michaelimani95@gmail.com>
2024-10-20 12:41:51 +0300

    trial 27

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Player from './components/Player.jsx';
+import GameBoard from './components/GameBoard.jsx';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
           <Player initialName="Player 1" symbol="X" />
           <Player initialName="Player 2" symbol="O" />
         </ol>
+        <GameBoard />
       </div>
     </main>
   );
